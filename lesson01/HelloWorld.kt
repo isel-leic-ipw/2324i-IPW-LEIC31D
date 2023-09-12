6690c28b88373a5b08905f86a872faba2dcff760
Luís Falcão <lfalcao@cc.isel.ipl.pt>
2023-09-12 04:00:10 +0100

    Lesson 01: JavaScript introduction. Primitive types.

---
+++ lesson01/HelloWorld.kt
@@ -0,0 +1,15 @@
+class A(var x: Int)
+
+
+fun main() {
+    println("Olá Glorioso mundo onde o Maior clube do mundo existe e tem mais títulos que todos os outros")
+
+    val a = A(5)
+
+    a.X = 10
+
+    println(a.x)
+}
+
+
+
---
+++ lesson01/hello-world.js
@@ -0,0 +1,23 @@
+console.log("Olá Glorioso mundo onde o Maior clube do mundo existe e tem mais títulos que todos os outros")
+
+class A {
+    constructor(x) {
+        this.x = x
+    }
+}
+
+const a = new A(5)
+
+a.X = 10
+
+console.log(a.x)
+console.log(a)
+
+//console.log = "SLB"
+
+console.log(a)
+
+const b = 123
+
+
+
