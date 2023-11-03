2e799c408e1f00179fd992f872f242a924b42453
Luís Falcão <lfalcao@cc.isel.ipl.pt>
2023-11-03 14:20:46 +0000

    Lesson 13: Tasks Web API defined with openAPI file and implemented with Express, having tasks-server and tasks-web-api files.

---
+++ lesson13/tasks-server.mjs
@@ -0,0 +1,37 @@
+
+import cors from 'cors'
+import express from 'express'
+import * as tasksApi from './tasks-web-api.mjs'
+
+const PORT = 1904
+
+console.log("Setting up server")
+let app = express()
+
+// Contained resources
+// - Tasks: /tasks
+// - Task:  /tasks/:id
+
+
+app.use(cors())
+
+app.use(express.json())
+
+// Get All Tasks: GET /tasks
+app.get('/tasks', tasksApi.getAllTasks)
+
+// Get one Task: GET /tasks/:id
+app.get('/tasks/:id', tasksApi.getTask)
+
+// Insert Task: POST /tasks
+app.post('/tasks', tasksApi.insertTask)
+
+// Update Task: PUT /tasks/:id
+app.put('/tasks/:id', tasksApi.updateTask)
+
+// Delete Task: DELETE /tasks/:id
+app.delete('/tasks/:id', tasksApi.deleteTask)
+
+app.listen(PORT, () => console.log(`Server listening in http://localhost:${PORT}`))
+
+console.log("End setting up server")
